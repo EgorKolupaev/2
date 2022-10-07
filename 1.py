48553f63e92955a3095e6afee167de8fe576e6a7
e.kolupaev <almsivisotas2133@gmail.com>
2022-10-07 15:48:49 +0300

    add file

---
+++ 1.py
@@ -0,0 +1,7 @@
+import math
+a=float(input("введите значение угла в градусах\n"))
+a=math.radians(a)
+sina=math.sin(a)
+cosa=math.cos(a)
+tana=math.tan(a)
+print(f"синус = {sina}, косинус = {cosa}, тангенс = {tana}" )
